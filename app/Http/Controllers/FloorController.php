62b09efac6e5650adc27a7e3830f4c5d6afd07ff
mjsimo99 <mohashark21@gmail.com>
2023-05-16 15:49:25 +0100

    fix method foreach

--- app/Http/Controllers/FloorController.php
+++ app/Http/Controllers/FloorController.php
@@ -15,43 +15,8 @@ class FloorController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->wantsJson()) {
-            $floors = new Floor();
-            $limit = 10;
-            $offset = 0;
-            $search = [];
-            $where = [];
-            $with = [];
-            $join = [];
-            $orderBy = [];
-
-            if ($request->input('length')) {
-                $limit = $request->input('length');
-            }
-
-            if ($request->input('order')[0]['column'] != 0) {
-                $column_name = $request->input('columns')[$request->input('order')[0]['column']]['name'];
-                $sort = $request->input('order')[0]['dir'];
-                $orderBy[$column_name] = $sort;
-            }
-
-            if ($request->input('start')) {
-                $offset = $request->input('start');
-            }
-
-            if ($request->input('search') && $request->input('search')['value'] != "") {
-                $search['name'] = $request->input('search')['value'];
-            }
-
-            if ($request->input('where')) {
-                $where = $request->input('where');
-            }
-
-            $floors = $floors->getDataForDataTable($limit, $offset, $search, $where, $with, $join, $orderBy,  $request->all());
-            return response()->json($floors);
-        }
-
-        return view('content.floors.list');
+       $floors = Floor::all();
+        return view('content.floors.list', compact('floors'));
     }
 
     /**
--- resources/views/content/floors/list.blade.php
+++ resources/views/content/floors/list.blade.php
@@ -1,21 +1,34 @@
 @extends('layouts.app')
 @section('title', ' - Floor List')
 @section('content')
-<div class="container-fluid mb100">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    {{ __('Floor List') }}
-                    <a class="btn btn-sm btn-primary pull-right" href="{{ route('floors.create') }}">Create
-                        new</a>
+<div class="container-fluid mb100" bis_skin_checked="1">
+    <div class="row justify-content-center" bis_skin_checked="1">
+        <div class="col-md-12" bis_skin_checked="1">
+            <div class="card" bis_skin_checked="1">
+                <div class="card-header" bis_skin_checked="1">
+                    Floor List
+                    <a class="btn btn-sm btn-primary pull-right" href="{{ route('floors.create') }}">Create new</a>
+                    new</a>
                 </div>
 
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table id="floorDatatable" class="table table-borderd table-condenced w-100">
+                <div class="card-body" bis_skin_checked="1">
+                    <div class="table-responsive" bis_skin_checked="1">
+                        <div id="floorDatatable_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer" bis_skin_checked="1"><div class="row" bis_skin_checked="1"><div class="col-12 col-sm-6" bis_skin_checked="1"><div class="dataTables_length" id="floorDatatable_length" bis_skin_checked="1"><label>Show <select name="floorDatatable_length" aria-controls="floorDatatable" class="form-control form-control-sm"><option value="10">10</option><option value="50">50</option><option value="100">100</option><option value="200">200</option><option value="-1">All</option></select> entries</label></div></div><div class="col-12 col-sm-6" bis_skin_checked="1"><div id="floorDatatable_filter" class="dataTables_filter" bis_skin_checked="1"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="floorDatatable"></label></div></div></div><div class="row" bis_skin_checked="1"><div class="col-12 col-sm-12" bis_skin_checked="1"><table id="floorDatatable" class="table table-borderd table-condenced w-100 dataTable no-footer" role="grid" aria-describedby="floorDatatable_info">
 
-                        </table>
+                        <thead>
+                            <tr role="row">
+                                <th class="no-sort sorting_asc" rowspan="1" colspan="1" style="width: 50px;" aria-label="id">id</th><th class="sorting" tabindex="0" aria-controls="floorDatatable" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending">Name</th><th class="sorting" tabindex="0" aria-controls="floorDatatable" rowspan="1" colspan="1" aria-label="Remarks: activate to sort column ascending">Remarks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($floors as $floor)
+                            <tr>
+                                <td>{{ $floor->id }}</td>
+                                <td>{{ $floor->name }}</td>
+                                <td>{{ $floor->remarks }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
                     </div>
                 </div>
             </div>
@@ -24,6 +37,6 @@
 </div>
 @endsection
 
-@push('scripts')
-<script src="{{ asset('js/custom/settings/floor.js') }}"></script>
-@endpush
+
+
+
