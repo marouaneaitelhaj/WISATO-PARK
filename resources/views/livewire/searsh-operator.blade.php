01609029c83e6ec4681f9d3d65e6ae2d4dc124db
mjsimo99 <mohashark21@gmail.com>
2023-05-22 15:04:28 +0100

    marge

--- resources/views/livewire/searsh-operator.blade.php
+++ resources/views/livewire/searsh-operator.blade.php
@@ -6,29 +6,22 @@
     </div>
     <div class="form-group w-100 mx-4">
         <label for="identity" class="text-md-right">{{ __('Select Operator') }}</label>
-
-
+    
         <div class="form-check" id="operatorContainer">
             @foreach ($operators as $index => $operator)
-                <div class="form-check-inline" style="{{ $index >= 3 ? 'display:none;' : '' }}">
-                    <input class="form-check-input" type="checkbox" name="operator[]" value="{{ $operator->id }}"
-                        id="{{ $operator->id }}">
-                    <label class="form-check-label" for="{{ $operator->id }}">
-                        {{ $operator->name }}
-                    </label>
-                </div>
+            <div class="form-check-inline" style="{{ $index >= 3 ? 'display:none;' : '' }}">
+                <input class="form-check-input" type="checkbox" name="operator[]" value="{{ $operator->id }}" id="{{ $operator->id }}">
+                <label class="form-check-label" for="{{ $operator->id }}">
+                    {{ $operator->name }}
+                </label>
+            </div>
             @endforeach
         </div>
-
+    
         <button id="showMoreBtn" class="btn btn-primary" type="button">Show More</button>
         <button id="showLessBtn" class="btn btn-primary" type="button" style="display: none;">Show Less</button>
-
-
-
-
-
     </div>
-
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
@@ -37,7 +30,7 @@
                 $(this).hide();
                 $('#showLessBtn').show();
             });
-
+    
             $('#showLessBtn').on('click', function() {
                 $('.form-check-inline:gt(2)').hide();
                 $(this).hide();
@@ -45,4 +38,6 @@
             });
         });
     </script>
+    
+    
 </div>
