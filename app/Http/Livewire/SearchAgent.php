1803966a0cf82e805efeb0d06b0cd7c73a6874fe
mjsimo99 <mohashark21@gmail.com>
2023-05-24 15:40:23 +0100

    fix the search agent partie back

--- app/Http/Livewire/SearchAgent.php
+++ app/Http/Livewire/SearchAgent.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use App\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Session;
+
 
 class SearchAgent extends Component
 {
@@ -14,26 +16,47 @@ class SearchAgent extends Component
 
     public function mount()
     {
-        $this->agents = User::whereHas('roles', function ($query) {
-            $query->where('name', 'chef zone');
-        })->where('name', 'like', '%' . $this->search . '%')->take(5)->get();
+        $this->fetchAgents();
     }
 
     public function updatedSearch()
     {
-        $this->agents = User::whereHas('roles', function ($query) {
-            $query->where('name', 'chef zone');
-        })->where('name', 'like', '%' . $this->search . '%')->take(5)->get();
+        $this->fetchAgents();
     }
 
     public function selectAgent($agentId)
     {
         $agent = User::find($agentId);
-        if ($agent) {
+        if ($agent && !$this->isAgentSelected($agentId)) {
             $this->selectedAgents[] = $agent;
+            $this->sweetAlert('success', 'Agent selected successfully!');
+        } else {
+            $this->sweetAlert('error', 'Agent already selected!');
         }
-        $this->selectedAgent = null;
         
+        $this->selectedAgent = null;
+    }
+
+
+    private function isAgentSelected($agentId)
+    {
+        return collect($this->selectedAgents)->contains(function ($selectedAgent) use ($agentId) {
+            return $selectedAgent['id'] == $agentId;
+        });
+    }
+    private function sweetAlert($type, $message)
+    {
+        $this->dispatchBrowserEvent('show-sweet-alert', [
+            'type' => $type,
+            'message' => $message
+        ]);
+    }
+
+
+
+    public function removeAgent($agentId)
+    {
+        unset($this->selectedAgents[$agentId]);
     }
 
     public function render()
@@ -43,5 +66,12 @@ class SearchAgent extends Component
             'agents' => $this->agents,
             'selectedAgents' => $this->selectedAgents,
         ]);
+    }
+
+    private function fetchAgents()
+    {
+        $this->agents = User::whereHas('roles', function ($query) {
+            $query->where('name', 'chef zone');
+        })->where('name', 'like', '%' . $this->search . '%')->take(3)->get();
     }
 }
--- resources/views/livewire/search-agent.blade.php
+++ resources/views/livewire/search-agent.blade.php
@@ -1,21 +1,69 @@
 <div class="border form-group">
-    <div class="d-flex border w-100 justify-content-evenly">
-        <div class="w-100 d-flex">
-            <ul class="d-flex" style="list-style: none;">
-                @foreach ($selectedAgents as $index => $agent)
-                <li class="bg-success px-2 m-1 py-1 rounded">{{ $agent['name'] }}</li>
-                @endforeach
-            </ul>
-            <input type="text" style="border: none;" wire:model="search" class="form-control{{ $errors->has('agent') ? ' is-invalid' : '' }}" name="agent">
-        </div>
+    @if (session()->has('flash_message'))
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            Swal.fire({
+                icon: '{{ session('flash_message.type') }}',
+                text: '{{ session('flash_message.message') }}'
+            });
+        </script>
+    @endif
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        window.addEventListener('show-sweet-alert', event => {
+            Swal.fire({
+                icon: event.detail.type,
+                text: event.detail.message
+            });
+        });
+    </script>
+
+    <div class="d-flex flex-wrap border w-100 justify-content-between align-items-center">
+        <ul class="d-flex flex-wrap list-unstyled m-0">
+            @foreach ($selectedAgents as $index => $agent)
+                <li style="background-color: #2dce89;" class="px-2 m-1 py-1 rounded d-flex">
+                    <div>
+                        {{ $agent['name'] }}
+                    </div>
+                    <a type="button" wire:click="removeAgent({{ $index }})" class="" style="color: azure"><i
+                            class="fa fa-minus-circle mx-1" aria-hidden="true"></i>
+                    </a>
+                </li>
+            @endforeach
+            <input type="text" style="border: none;" wire:model="search" class="flex-grow-1">
+        </ul>
     </div>
     <div class="w-100">
         <div class="border">
-            @foreach ($agents as $index => $agent)
-            <div wire:click="selectAgent({{ $agent->id }})" class="border">
-                <p>{{ $agent->name }}</p>
+
+            <div class="border">
+                @foreach ($agents as $index => $agent)
+                <div wire:click="selectAgent({{$agent->id}})" style="cursor: pointer;" class="border tags d-flex align-items-center">
+                    <span>
+                        {{$agent->name}}
+                    </span>
+                </div>
+                @endforeach
+                
             </div>
-            @endforeach
+            
         </div>
     </div>
 </div>
+
+<style>
+    .tags {
+        padding: 5px 10px;
+        border-radius: 5px;
+        margin: 5px;
+    }
+
+    .tags:hover {
+        background-color: #2dce89;
+        color: white;
+    }
+
+    .tags span {
+        font-size: 14px;
+    }
+</style>
